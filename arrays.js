2dfef9c5f1cc0741797238f094480cb05f4005b5
Bielefeld <thejens90@gmail.com>
2021-05-27 20:53:53 +0200

    added new quotes, added fav song calls

--- arrays.js
+++ arrays.js
@@ -3,13 +3,17 @@
 //all the famos quotes of Jimmy 
 const jimmyquotes = [
 	'\"This is the last song.\" - Jimmy (This was not the last song)',
-	'\"I love this song!\" - Jimmy',
+	'\"Uhhh I love this song!\" - Jimmy',
 	'\"This really is the last song! I swear\" - Jimmy',
 	'\"This is my favourite Song!\" - Jimmy',
+	'\"This is one of my favourite Song!\" - Jimmy',
 	'\"Nonono, I will not stream for 8 hours." - Jimmy, streams for 8 hours anyway.',
-	'\"What! Wrong button again?\" - Jimmy',
+	'\"How does that work??\" - Jimmy',
 	'\"Sorry wrong button...\" - Jimmy, while pressing wrong buttons',
 	'\"Man, I love kirin beer\" - Jimmy, every time',
+	'\"You are awesome!\" - Jimmy to you',
+	'\"Damn I am smooth today\" - Jimmy, really smooth today',
+	'\"Hey, Do you have your Beats on?\" - Jimmy',
 ]
 //ofc is this the last song? jeez. 
 const randomLastSongCall = [
@@ -20,6 +24,13 @@ const randomLastSongCall = [
 	'I swear! This is the last song.',
 	'The last song! I am not touching any more buttons.',
 	'Hahaha the last song xD That was a good one.',
+]
+
+//all the times he loves this song
+const favSongCall = [
+	'This song. Thats my favorite!',
+	'This is one of my favorite songs.',
+	'Uhh I love this song.',
 ]
 
 //returns one random item of Jimmys quotes
@@ -33,7 +44,14 @@ function getRandomLastSongCall(){
     return randomLastSongCall[Math.floor(Math.random() * randomLastSongCall.length)];
 }
 
+//returns one random item of the favsong Array
+function getFavSongCall()
+{
+	return favSongCall[Math.floor(Math.random() * favSongCall.length)];
+}
+
 module.exports = {
     getRandomJimmyQuote,
-    getRandomLastSongCall
+    getRandomLastSongCall,
+	getFavSongCall
 };
