baf08fd8b1de330a305586440c4727b67d62624c
Bielefeld <thejens90@gmail.com>
2021-06-03 22:51:12 +0200

    added new quotes

--- messages/randommessagearrays.js
+++ messages/randommessagearrays.js
@@ -4,16 +4,19 @@
 const jimmyquotes = [
 	'\"This is the last song.\" - Jimmy (This was not the last song)',
 	'\"Uhhh I love this song!\" - Jimmy',
-	'\"This really is the last song! I swear\" - Jimmy',
+	'\"Kareen is in stelth mode again, she is our ninja!\" - Jimmy',
 	'\"This is my favourite Song!\" - Jimmy',
-	'\"This is one of my favourite Song!\" - Jimmy',
 	'\"Nonono, I will not stream for 8 hours." - Jimmy, streams for 8 hours anyway.',
+	'\"@tuinkabouter1965 She is my captian\" - Jimmy',
 	'\"How does that work??\" - Jimmy',
+	'\"@salad_lee. She is a chinese in germany or so idk. I don\'t even know if salad even is a she.\" - Jimmy',
 	'\"Sorry wrong button...\" - Jimmy, while pressing wrong buttons',
 	'\"Man, I love kirin beer\" - Jimmy, every time',
 	'\"You are awesome!\" - Jimmy to you',
+	'\"Professor. He is the IT guy here. He made the bot GrandpaJimmyT!\" - Jimmy',
 	'\"Damn I am smooth today\" - Jimmy, really smooth today',
 	'\"Hey, Do you have your Beats on?\" - Jimmy',
+	'\"How do i look guys?\" - Jimmy',
 ]
 //ofc is this the last song? jeez. 
 const randomLastSongCall = [
